e15a8c9f036478baf2ee43c30c233d8ba44e2193
jaspreet <ghotra.jaspreet003@gmail.com>
2024-03-08 18:39:06 -0500

    fibonacci problem solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
 
+    let previousNumber = 0;
+    let currentNumber = 1;
+    let sum = 0;
+    if (number == 1) {
+        sum = 1
+    } else if (number < 0) {
+        sum = 'OOPS'
+    } else {
+        for (let i = 1; i < number; i++) {
+
+            sum = previousNumber + currentNumber
+
+            previousNumber = currentNumber;
+
+            currentNumber = sum;
+        }
+    }
+    return sum;
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
