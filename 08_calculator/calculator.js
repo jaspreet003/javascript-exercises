2beb22f3f2599ff062d3470e4adcb53821c16629
jaspreet <ghotra.jaspreet003@gmail.com>
2024-03-08 17:23:13 -0500

    q-8 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,86 @@
-const add = function() {
-	
+const sum = function (...args) {
+  let sum = 0;
+
+  for (let i = 0; i < args.length; i++) {
+
+    if (Array.isArray(args[i])) {
+
+      if (args[i].length > 0) {
+
+        sum += args[i].reduce((a, b) => {
+
+          return a + b;
+        })
+      } else { sum += 0 }
+
+    } else {
+      if (typeof args[i] === 'number') {
+        sum += args[i];
+      }
+    }
+  }
+
+  return sum;
 };
 
-const subtract = function() {
-	
-};
 
-const sum = function() {
-	
-};
+const subtract = function (a, b) {
 
-const multiply = function() {
+  return a - b;
 
 };
 
-const power = function() {
-	
+const add = function (a, b) {
+
+  return a + b;
+
 };
 
-const factorial = function() {
-	
+const multiply = function (...args) {
+  let product = 1;
+
+  for (let i = 0; i < args.length; i++) {
+
+    if (Array.isArray(args[i])) {
+
+      if (args[i].length > 0) {
+
+        product *= args[i].reduce((a, b) => {
+
+          return a * b;
+        })
+      } else { product *= 1 }
+
+    } else {
+      if (typeof args[i] === 'number') {
+        product *= args[i];
+      }
+    }
+  }
+
+  return product;
+};
+
+const power = function (number, power) {
+
+  let result = 1;
+
+  for (let i = 0; i < power; i++) {
+    result *= number;
+  }
+
+  return result;
+};
+
+const factorial = function (number) {
+
+  let result = 1;
+  for (let i = 1; i <= number; i++) {
+    result *= i;
+  }
+
+  return result;
+
 };
 
 // Do not edit below this line
