7df93abe6fbeffc58dc03322c79b25920290f044
jaspreet <ghotra.jaspreet003@gmail.com>
2024-03-08 18:16:37 -0500

    palidrome problem solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (value) {
+
+    let clearString = value.replace(/[^a-zA-Z0-9]/g, '');
+
+    return clearString.split('').reverse().join('').toUpperCase() === clearString.toUpperCase() ? true : false;
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
