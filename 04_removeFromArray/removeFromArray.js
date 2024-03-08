2572f6cf3cd4d0db6d2fdf0c43e48ad8dadf3d67
jaspreet <ghotra.jaspreet003@gmail.com>
2024-03-08 14:01:10 -0500

    solve q-5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,32 +1,8 @@
-const removeFromArray = function (arr, valueToRemove) {
-
-    let resultArr = [];
-    let arrOfValueToRemove = "";
-    arrOfValueToRemove = valueToRemove;
-
-    console.log(typeof valueToRemove, valueToRemove);
-
-    if (arr.includes(',')) {
-        arrOfValueToRemove = valueToRemove.split(',');
-    }
-    console.log(arrOfValueToRemove);
-
-    // for (let i = 0; i < arr.length; i++) {
-    //     // console.log(i, arr[i]);
-
-    //     if (arr[i] === valueToRemove) {
-    //         // console.log(arr, valueToRemove)
-    //         resultArr = arr.toSpliced(i, 1);
-    //         // console.log(resultArr)
-
-    //     }
-
-    // }
-
-    // return resultArr;
+const removeFromArray = function (arr, ...args) {
+    
+    return arr.filter(val => !args.includes(val))
 };
 
-removeFromArray([1, 2, 3], 3, 4);
 
 // Do not edit below this line
-//module.exports = removeFromArray;
+module.exports = removeFromArray;
