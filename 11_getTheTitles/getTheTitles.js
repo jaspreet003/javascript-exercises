dc0bce1b593fd9ca0c70de246164f82a2497fd40
jaspreet <ghotra.jaspreet003@gmail.com>
2024-03-08 19:22:23 -0500

    value of object in array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (arr) {
+    let arrOfTitles = arr.map((obj, index) => {
+        return arr[index].title;
+    })
 
+    return arrOfTitles;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
